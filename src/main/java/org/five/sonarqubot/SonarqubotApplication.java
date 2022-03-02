0ba558d2ea5244251caf7b3e293f153974842e5c
DoNotWorr <alexander.wiklund1@gmail.com>
2022-03-02 17:33:25 +0100

    Generated project with Spring Initilizer

---
+++ src/main/java/org/five/sonarqubot/SonarqubotApplication.java
@@ -0,0 +1,13 @@
+package org.five.sonarqubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/five/sonarqubot/SonarqubotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.five.sonarqubot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
