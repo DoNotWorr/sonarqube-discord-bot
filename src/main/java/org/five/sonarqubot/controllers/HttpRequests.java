1b1c6258b6b802585bef484c01a2bbe07bb9cd12
IsaLjungdahl <isa.ljungdahl@gmail.com>
2022-05-27 16:20:32 +0200

    Postmapping to delete by project, delete all and Getmapping to get info about existing projects and analyses

--- src/main/java/org/five/sonarqubot/controllers/HttpRequests.java
+++ src/main/java/org/five/sonarqubot/controllers/HttpRequests.java
@@ -8,17 +8,16 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HttpRequests {
-    @GetMapping("/hello-world")
+    @GetMapping("/")
     public String helloWorld() {
         return "Hello World! ";
     }
 
-    @GetMapping("/restController")
-    public ResponseEntity<Object> restController() {
-
+    @GetMapping("/search")
+    public ResponseEntity<Object> getProjects() {
 
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:9000/api/webservices/list";
+        String url = "http://localhost:9000/api/projects/search";
         ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.GET, authHeader(), Object.class);
         response.getBody();
         return response;
@@ -32,7 +31,44 @@ public class HttpRequests {
         String url = "http://localhost:9000/api/projects/create" +
                 "?name=" + name + "&project=" + project + "&visibility=" + visibility;
 
-        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, authHeader(), Object.class,HttpStatus.CREATED);
+        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, authHeader(), Object.class, HttpStatus.CREATED);
+        response.getBody();
+        return response;
+
+    }
+
+    @GetMapping("/analyses")
+    public ResponseEntity<Object> getAnalysesByProject(@RequestParam String project) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:9000/api/measures/search_history" + "?component=" + project + "&metrics=" + metrics();
+        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.GET, authHeader(), Object.class);
+        response.getBody();
+        return response;
+
+    }
+
+    /*To delete a project you need the project's key, this is the value in "project" when creating a project */
+    @PostMapping(path = "/delete")
+    public ResponseEntity<Object> deleteByProject(@RequestParam String project) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:9000/api/projects/delete" +
+                "?project=" + project;
+
+        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, authHeader(), Object.class, HttpStatus.CREATED);
+        response.getBody();
+        return response;
+
+    }
+    //TODO= deleteAll needs "At least one parameter among analyzedBefore, projects and q must be provided"
+    @PostMapping(path = "/deleteAll")
+    public ResponseEntity<Object> deleteAll() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:9000/api/projects/bulk_delete";
+
+        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, authHeader(), Object.class);
         response.getBody();
         return response;
 
@@ -40,7 +76,7 @@ public class HttpRequests {
 
 
     public HttpEntity<String> authHeader() {
-        String plainCreds = "admin:isa";
+        String plainCreds = "admin:admin";
         byte[] plainCredsBytes = plainCreds.getBytes();
         byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
         String base64Creds = new String(base64CredsBytes);
@@ -48,6 +84,12 @@ public class HttpRequests {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Basic " + base64Creds);
         return new HttpEntity<>(headers);
+    }
+
+    public String metrics() {
+        String[] metricsArray = {"coverage", "bugs", "new_violations", "lines", "statements"};
+        String metrics = String.join(",", metricsArray);
+        return metrics;
     }
 
 }
