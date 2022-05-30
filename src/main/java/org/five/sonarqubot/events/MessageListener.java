3378340367b1a147a8ea3d031cbea4f55387af8c
Abdi <abdi_665@hotmail.com>
2022-05-31 00:01:14 +0200

    Fixed so the bot can recognize code blocks and pings on discord chats

--- src/main/java/org/five/sonarqubot/events/MessageListener.java
+++ src/main/java/org/five/sonarqubot/events/MessageListener.java
@@ -4,13 +4,15 @@ import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
 
 public abstract class MessageListener {
+    public static String ping = "!ping";
+    public static String code = "```!code```";
 
     public Mono<Void> processCommand(Message eventMessage) {
         return Mono.just(eventMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .filter(message -> message.getContent().equalsIgnoreCase(ping) || message.getContent().equalsIgnoreCase(code))
                 .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Pongelipong!\n" +
+                .flatMap(channel -> channel.createMessage("Pongelipong! \n" +
                 "Your wish is my command\n"
                 ))
                 .then();
